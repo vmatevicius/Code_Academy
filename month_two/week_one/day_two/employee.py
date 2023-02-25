1a5a08c4afc90e77297a06075ad1b2cc926e2280
vmatevicius <107873896+vmatevicius@users.noreply.github.com>
2023-02-25 17:28:48 +0200

    Update employee.py

--- month_two/week_one/day_two/employee.py
+++ month_two/week_one/day_two/employee.py
@@ -1,6 +1,4 @@
-from typing import Union, Optional
 import logging 
-
 
 # Create the instance attributes fullname and email in the Employee class. Given a person's first and last names:
 # Form the fullname by simply joining the first and last name together, separated by a space.
