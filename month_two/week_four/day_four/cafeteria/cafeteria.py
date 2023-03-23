cd68914c8457bfc866cab3a816f6627676ca394e
vmatevicius <vytis765@gmail.com>
2023-03-23 21:04:59 +0200

    py papa para po

---
+++ month_two/week_four/day_four/cafeteria/cafeteria.py
@@ -0,0 +1,19 @@
+# Cafeteria project : Create an live menu and payment system (a.k.a console program) :
+
+# First the program should ask if table was reserved/ not (Providing your full name) .
+# And then if not would assign you a table (there is a specific number single, double or family tables) .
+# After table is assigned to you, system should show how many free tables are and how which are reserved/occupied.
+# The system must be able to show name/surname of the person of the reserved table (CLI option : enter reserved table nuber ; OUTCOME: Name/Surname/Time Reserved)
+
+# After assigning table, system should show different menu options for breakfast, lunch , dinner , drinks (alcohol.
+# alcohol free), to choose from. Special menu for vegetarian and vegan must be included too in the special menu.
+# All menu items should have weight, preparation time in minutes, calories, and price.
+
+# I have to have an option to change the order before the payment section. Thus I can delete, add more, update amount of the same order.
+
+# I should be able to choose whatever I want from all menus in one ordering.
+# After I finish, I need to see what I chosen, the full payable amount and approx waiting time for the food to be served
+
+# Add an option to add tips (% from the full cost) to the final bill.
+
+# After the payment , system should generate the receipt (logging).
