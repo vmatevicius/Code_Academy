14572beb8c0973ddca2ab95d9c815add929243e0
vmatevicius <vytis765@gmail.com>
2023-03-28 21:33:17 +0300

    Param param

---
+++ month_three/week_one/functional_programming/main.py
@@ -0,0 +1,21 @@
+# Lambdas section:
+
+# Write a Python program to find if a given string starts with a given character using Lambda. Sample Output: True False
+# Write a Python program to create a lambda function that adds 15 to a given number passed in as an argument,
+# also create a lambda function that multiplies argument x with argument y and print the result.
+# Write a Python program to add two given lists using map and lambda.
+
+# Write a Python program to square and cube every number in a given list of integers using Lambda
+
+# variable = lambda x: True if x.startswith("P") else False
+# print(variable("Antanas"))
+
+# print((lambda number: number + 15)(15))
+
+# print((lambda x, y: x * y)(15, 15))
+
+# print(list((map(lambda list_one, list_two: list_one + list_two, [1, 2], [3, 4]))))
+
+# print(list((map(lambda numbers: numbers**2, [1, 2, 3, 4]))))
+
+# print(list((map(lambda numbers: numbers**3, [1, 2, 3, 4]))))
