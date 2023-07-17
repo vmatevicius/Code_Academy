2cd057dd0816c8623b7437950f4826d5bed8d35a
vmatevicius <vytis765@gmail.com>
2023-07-17 19:53:03 +0300

    Created base class and cpu class, added some methods

---
+++ revision/parts_data/cpu.py
@@ -0,0 +1,30 @@
+CPUS = [
+    {
+        "name": "AMD Ryzen 5 5600X",
+        "brand_name": "AMD",
+        "cores": 6,
+        "performance": "3.7 GHz",
+        "price": 162.66,
+    },
+    {
+        "name": "AMD Ryzen 7 5800X",
+        "brand_name": "AMD",
+        "cores": 8,
+        "performance": "3.8 GHz",
+        "price": 217.99,
+    },
+    {
+        "name": "Intel Core i7-13700K",
+        "brand_name": "Intel",
+        "cores": 16,
+        "performance": "3.4 GHz",
+        "price": 409.00,
+    },
+    {
+        "name": "AMD Ryzen 7 7800X3D",
+        "brand_name": "AMD",
+        "cores": 8,
+        "performance": "4.2 GHz",
+        "price": 439.00,
+    },
+]
